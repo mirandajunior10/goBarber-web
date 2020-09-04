9a20d900cfc8c391ed2ddaa78de0077cb0371d6c
Jorlan Souza de Miranda Júnior <jorlanjr@gmail.com>
2020-09-04 19:57:21 -0300

    Iniciando front-end web:  Módulo 1 - Aula 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-import SignIn from './pages/SignIn';
+import SignIn from './pages/SignUp';
 
-const App: React.FC = props => (
+const App: React.FC = () => (
   <>
     <SignIn />
     <GlobalStyle />
