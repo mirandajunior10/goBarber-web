a551efe617cf0ef1583d1cf58740f3350d908ee4
Jorlan Souza de Miranda Júnior <jorlanjr@gmail.com>
2020-09-04 21:36:34 -0300

    Iniciando front-end web:  Módulo 1 - Aula 7

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,19 +1,36 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
-  color: #666360;
-  border: 2px solid #232129;
   padding: 16px;
   width: 100%;
+
+  border: 2px solid #232129;
+  color: #666360;
+
   display: flex;
   align-items: center;
 
   & + div {
     margin-top: 8px;
   }
-
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9900;
+      border-color: #ff9900;
+    `}
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9900;
+    `}
   input {
     background: transparent;
     flex: 1;
