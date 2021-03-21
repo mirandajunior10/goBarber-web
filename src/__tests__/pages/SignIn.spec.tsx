fbc9177151fa2a939b07c36ca717c14b52cef33d
Jorlan Souza de Miranda Júnior <jorlanjr@gmail.com>
2021-03-21 12:12:51 -0300

    Testes no ReactJS: Módulo 1 - Teste de login

--- src/__tests__/pages/SignIn.spec.tsx
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import SignIn from '../../pages/SignIn';
+
+const mockedHistoryPush = jest.fn();
 
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 describe('SignIn page', () => {
   it('should be able to sign in', () => {
-    jest.spyOn;
-    const { debug } = render(<SignIn />);
-    debug();
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, { target: 'johndoe@example.com' });
+    fireEvent.change(passwordField, { target: '123456' });
+
+    fireEvent.click(buttonElement);
+
+    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
   });
 });
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -67,7 +67,7 @@ const SignIn: React.FC = () => {
               name="password"
               icon={FiLock}
               type="password"
-              placeholder="senha"
+              placeholder="Senha"
             />
 
             <Button type="submit">Entrar</Button>
